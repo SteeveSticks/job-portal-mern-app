454545fa5cffa0e865a63451a4350fa4061d19a4
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-04-02 12:23:36 +0100

    recomemded update

--- frontend/src/pages/home/recomended.jsx
+++ frontend/src/pages/home/recomended.jsx
@@ -1,57 +1,60 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Recomended = () => {
   return (
     <>
-      <div className="flex py-36 px-24 gap-4">
+      <AnimatePresence>
         <motion.div
           initial={{ opacity: 0, y: 50 }} // start invisible and lower
-          animate={{ opacity: 1, y: 0 }} // fade in and move up
+          whileInView={{ opacity: 1, y: 0 }} // fade in and move up
           transition={{
             duration: 1,
             ease: "easeOut",
           }}
-          className="border px-6 py-6 w-96 h-64 rounded-sm shadow-sm"
+          viewport={{ once: false, amount: 0.2 }}
+          className="flex py-36 px-24 gap-4"
         >
-          <span className="text-secondary text-xl font-bold">1</span>
+          <motion.div className="border px-6 py-6 w-96 h-64 rounded-sm shadow-sm">
+            <span className="text-secondary text-xl font-bold">1</span>
 
-          <h1 className="font-bold text-primary text-2xl mt-5 mb-5">
-            Create your <br />
-            free account
-          </h1>
-          <p className="text-gray-600">
-            All you need is your email address to create an account and start
-            applying for job post.
-          </p>
+            <h1 className="font-bold text-primary text-2xl mt-5 mb-5">
+              Create your <br />
+              free account
+            </h1>
+            <p className="text-gray-600">
+              All you need is your email address to create an account and start
+              applying for job post.
+            </p>
+          </motion.div>
+
+          <div className="border px-6 py-6 w-96 h-64 rounded-sm shadow-sm">
+            <span className="text-secondary text-xl font-bold">2</span>
+
+            <h1 className="font-bold text-primary text-2xl mt-5 mb-5">
+              For employers build your <br />
+              job post
+            </h1>
+            <p className="text-gray-600">
+              Then just add a title, description, and location to your job post,
+              and you're ready to go.
+            </p>
+          </div>
+
+          <div className="border px-6 py-6 w-96 h-64 rounded-sm shadow-sm">
+            <span className="text-secondary text-xl font-bold">3</span>
+
+            <h1 className="font-bold text-primary text-2xl mt-5 mb-5">
+              Post <br />
+              your job
+            </h1>
+            <p className="text-gray-600">
+              After you post your job use our state of the art tools to help you
+              find dream talent.
+            </p>
+          </div>
         </motion.div>
-
-        <div className="border px-6 py-6 w-96 h-64 rounded-sm shadow-sm">
-          <span className="text-secondary text-xl font-bold">2</span>
-
-          <h1 className="font-bold text-primary text-2xl mt-5 mb-5">
-            For employers build your <br />
-            job post
-          </h1>
-          <p className="text-gray-600">
-            Then just add a title, description, and location to your job post,
-            and you're ready to go.
-          </p>
-        </div>
-
-        <div className="border px-6 py-6 w-96 h-64 rounded-sm shadow-sm">
-          <span className="text-secondary text-xl font-bold">3</span>
-
-          <h1 className="font-bold text-primary text-2xl mt-5 mb-5">
-            Post <br />
-            your job
-          </h1>
-          <p className="text-gray-600">
-            After you post your job use our state of the art tools to help you
-            find dream talent.
-          </p>
-        </div>
-      </div>
+      </AnimatePresence>
     </>
   );
 };
